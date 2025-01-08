d47a62b1f5a56d5fe9f749812a6c089d18aca6c3
Jesna Varghese <jesna.varghese@dbizsolution.com>
2025-01-08 12:00:59 +0530

    some files created and edited

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Header} from './Components/Header';
 import {Footer} from './Components/Footer';
-import {} from './Components/ProductDetails'
-import {ProductPage} from './pages/ProductPage';
+import {ProductList} from './pages/ProductList';
 import "./App.css";
-import {CollectionsPage} from './pages/ProductList/CollectionPage'
-import {DeliveryDetails} from './pages/ProductList/DeliveryDetails'
 import {DetailFooter} from './Components/DetailFooter'
 import { ProductDetails } from "./Components/ProductDetails";
 
@@ -18,14 +15,14 @@ function App() {
       <div className="App">
         
         <Header />
-        <CollectionsPage />
+        
         <main>
           <Routes>
-            <Route path="/shop" element={<ProductPage />} />
+            <Route path="/shop" element={<ProductList />} />
             <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
         </main>
-        <DeliveryDetails/>
+       
         <DetailFooter/>
         <Footer />
       </div>
--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,13 +1,24 @@
 import React from "react";
 import {products} from '../../data/Products'
 import {ProductCard} from '../../Components/ProductCard'
+import {CollectionsPage} from '../../Components/CollectionPage'
+import {DeliveryDetails} from '../../Components/DeliveryDetails'
 
-export const ProductPage = () => {
+
+
+export const ProductList = () => {
   return (
+    <div>
+      <CollectionsPage />
+    
     <div className="product-list">
+
+      
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
+    </div>
+    <DeliveryDetails/>
     </div>
   );
 };
